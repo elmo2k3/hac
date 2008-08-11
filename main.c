c7f35b409f98b5d84c891b4216a6435f9dec4d3f
Bjoern Biesenbach <bjoern@bjoern-b.de>
2008-08-11 19:27:03 +0200

    Umstrukturiert

--- main.c
+++ main.c
@@ -5,9 +5,28 @@
 int main(int argc, char *argv[])
 {
 	uint8_t foo;
+	float temperature;
+
+	initLibHac("192.168.0.2");
 
 	if(argc < 2)
+	{
+		printf("\nUsage: \
+				%s blink     Flash red\n \
+				%s sr NUM    Set relais to NUM\n \
+				%s gr        Get relais status\n \
+				%s tr NUM    Toggle relais\n \
+				\n \
+				%s sled RED GREEN BLUE SMOOTHNESS\n \
+				%s gt MODUL SENSOR\n\n",
+				argv[0],
+				argv[0],
+				argv[0],
+				argv[0],
+				argv[0],
+				argv[0]);
 		return 0;
+	}
 
 	if(!strcmp(argv[1],"blink"))
 		rgbBlink(0,0);
@@ -32,6 +51,15 @@ int main(int argc, char *argv[])
 				atoi(argv[5]));
 	}
 
+	else if(!strcmp(argv[1], "gt"))
+	{
+		getTemperature(atoi(argv[2]),
+				atoi(argv[3]),
+				&temperature);
+		printf("\n%3.2f\n",temperature);
+	}
+
+	closeLibHac();
 
 	return 0;
 }
